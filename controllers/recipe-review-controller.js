f3b7c7798ee9639385ab1756c2b2febf73bfde10
Calvin <clee2691@gmail.com>
2022-04-14 14:08:31 -0400

    Recipe review (#2)
    * Added recipe review functionality
    
    * Show recipes in home page/ profile
    
    * Fixed API links for recipe review

---
+++ controllers/recipe-review-controller.js
@@ -0,0 +1,50 @@
+import * as ReviewDao from "../database/recipe/review-dao.js";
+
+const ReviewController = (app) => {
+  app.get("/api/recipe/review/recent", findRecentReviews);
+  app.get("/api/recipe/review/meal/:mealId", findReviewsMealId);
+  app.get("/api/recipe/review/user/:uid", findRevsByUId);
+  app.post("/api/recipe/review", createRecipeReview);
+  app.delete("/api/recipe/review/:revId", deleteRecipeReview);
+};
+
+const findRecentReviews = async (req, res) => {
+  const recentReviews = await ReviewDao.findRecentReviews();
+  res.json(recentReviews);
+};
+
+const findReviewsMealId = async (req, res) => {
+  const mealId = req.params.mealId;
+  const mealReviews = await ReviewDao.findAllRevByMealId(mealId);
+  res.json(mealReviews);
+};
+
+const findRevsByUId = async (req, res) => {
+  const uid = req.params.uid;
+  const userReviews = await ReviewDao.findAllRevByUserId(uid);
+  res.json(userReviews);
+};
+
+const createRecipeReview = async (req, res) => {
+  const newReview = req.body;
+  const foundReview = await ReviewDao.findRevByMealIdUserId(
+    newReview.mealId,
+    newReview.userId
+  );
+
+  if (foundReview) {
+    res.sendStatus(403);
+    return;
+  }
+
+  const insertedReview = await ReviewDao.createReview(newReview);
+  res.json(insertedReview);
+};
+
+const deleteRecipeReview = async (req, res) => {
+  const { revId } = req.params;
+  const status = await ReviewDao.deleteReview(revId);
+  res.send(status);
+};
+
+export default ReviewController;
---
+++ database/recipe/review-dao.js
@@ -0,0 +1,16 @@
+import RecipeRevModel from "./review-model.js";
+
+export const findAllReviews = () => RecipeRevModel.find();
+
+export const findAllRevByMealId = (mealId) => RecipeRevModel.find({ mealId });
+
+export const findAllRevByUserId = (userId) => RecipeRevModel.find({ userId });
+
+export const findRevByMealIdUserId = (mealId, userId) => RecipeRevModel.findOne({mealId, userId});
+
+export const createReview = (review) => RecipeRevModel.create(review);
+
+export const deleteReview = (revId) =>
+  RecipeRevModel.deleteOne({ _id: revId });
+
+export const findRecentReviews = () => RecipeRevModel.find().sort({revDate: -1}).limit(5);
