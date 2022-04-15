9bc79c974352ab8a63cff8f5b629466c65e464c7
Calvin <clee2691@gmail.com>
2022-04-15 11:06:08 -0400

    Users (#5)
    * Sanitized user sensitive info
    
    * Removed comments

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,55 +1,60 @@
 import * as UserDao from "../database/users/user-dao.js";
 
 const userController = (app) => {
-    app.get('/api/users', findAllUsers);
-    app.get('/api/users/:uid', findUserById);
-    app.get("/api/users/email/:email", findUserByEmail);
-    
-    app.put("/api/users/:userId", updateUser);
-    app.delete("/api/users/:id", deleteUser);
-}
+  app.get("/api/users", findAllUsers);
+  app.get("/api/users/userId/:uid", findUserById);
+  app.get("/api/users/email/:email", findUserByEmail);
+
+  app.put("/api/users/:userId", updateUser);
+  app.delete("/api/users/:id", deleteUser);
+};
 
 const findAllUsers = async (req, res) => {
-    const users = await UserDao.findAllUsers();
-    res.json(users);
-}
+  const users = await UserDao.findAllUsers();
+  users.forEach(user => {
+    user.email = "";
+    user.password = "";
+    user.sensitiveInfo = {};
+    user.userStats = {};
+  });
+  res.json(users);
+};
 
 const findUserById = async (req, res) => {
-    const userId = req.params.uid;
-    const user = await UserDao.findUserById(userId);
-    if (user) {
-        res.json(user);
-    } else {
-        res.sendStatus(404);
-    }
-    
-}
+  const userId = req.params.uid;
+  const user = await UserDao.findUserById(userId);
+  if (user) {
+    res.json(user);
+  } else {
+    res.sendStatus(404);
+  }
+};
 
 const findUserByEmail = async (req, res) => {
-    const userEmail = req.params.email;
-    const user = await UserDao.findUserByEmail(userEmail);
-    if (user) {
-        res.json(user);
-    } else {
-        res.sendStatus(404);
-    }
-}
+  const userEmail = req.params.email;
+  const user = await UserDao.findUserByEmail(userEmail);
+  if (user) {
+    res.json(user);
+  } else {
+    res.sendStatus(404);
+  }
+};
 
 const updateUser = async (req, res) => {
-    const user = req.body;
-    const uid = req.params.userId;
-    const status = await UserDao.updateUser(uid, user);
-    // Set the session to new update user
-    const existingUser = await UserDao.findUserById(uid);
-    existingUser.password = "****";
-    req.session["profile"] = existingUser;
-    res.send(status);
+  const user = req.body;
+  const uid = req.params.userId;
+  const status = await UserDao.updateUser(uid, user);
+  // Set the session to new update user
+  const existingUser = await UserDao.findUserById(uid);
+  existingUser.password = "****";
+  req.session["profile"] = existingUser;
+  res.send(status);
 };
 
 const deleteUser = async (req, res) => {
-    const userId = req.params.id;
-    const status = await UserDao.deleteUser(userId);
-    res.send(status)
+  const userId = req.params.id;
+  const status = await UserDao.deleteUser(userId);
+  res.send(status);
 };
 
 export default userController;
